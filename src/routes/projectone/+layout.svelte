2140e48e3774d33efe2a21dc1900183b61a97103
Aravinda <apranthilaka@outlook.com>
2026-09-24 14:29:00 +0530

    update

--- src/routes/projectone/+layout.svelte
+++ src/routes/projectone/+layout.svelte
@@ -1,15 +1,6 @@
 <script>
-	import { fade } from 'svelte/transition';
 	let { children } = $props();
+	let name = $state('aravinda');
 </script>
 
-<div class="grid h-screen w-full grid-cols-[300px_1fr] grid-rows-[50px_1fr] border">
-	<div class="border">1</div>
-	<div class="border">2</div>
-	<div class="border">3</div>
-	<div class="border">
-		<div in:fade={{ duration: 1000 }}>
-			{@render children()}
-		</div>
-	</div>
-</div>
+{@render children()}
--- src/routes/projectone/+page.svelte
+++ src/routes/projectone/+page.svelte
@@ -1,3 +1,10 @@
-Main Page
-<br />
-<a href="/projectone/grid">Grid</a>
+<script>
+	let name = $state('aravinda');
+</script>
+
+<!-- main wrapper  -->
+<div
+	class="min-h-screen w-full bg-[url('https://images.unsplash.com/photo-1521080755838-d2311117f767?q=80&w=2762&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat"
+></div>
+
+<div>name</div>
