85ce3ec12f3d2a25808a575a6bbd674a9a329f9e
Aravinda <apranthilaka@outlook.com>
2026-09-24 12:58:20 +0530

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div
+	class="flex h-screen w-full flex-col items-center justify-center bg-slate-50 text-center text-sm"
+>
+	<div class="w-10">
+		<img src="logo.png" alt="" />
+	</div>
+	<div class="mb-2 text-lg font-medium">Launching Soon.</div>
+	<div class="max-w-110">
+		Interactive web implementations of my designs, developed through hybrid, manual coding and AI
+		collaboration.
+	</div>
+	⌁
+	<div class="">Sveltekit • Shadcn • Tailwind4</div>
+	<div class="">Claude • Ollama/Qwen code</div>
+	⌁
+	<div class="text-xs text-neutral-500">Status: Migrating component architecture</div>
+	⌁
+	<a
+		class="delay-00 rounded-sm border border-neutral-300 px-3 py-1 text-[12px] text-neutral-500 transition-all hover:bg-neutral-200"
+		href="/">Home</a
+	>
+</div>
