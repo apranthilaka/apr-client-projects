5f12a4110ba59a5d43359a96bbfd0c137aa24df4
Aravinda <apranthilaka@outlook.com>
2026-09-25 15:06:46 +0530

    update

--- src/routes/projectone/+page.svelte
+++ src/routes/projectone/+page.svelte
@@ -1,10 +1,37 @@
-<script>
-	let name = $state('aravinda');
-</script>
+<div class=" flex h-auto w-full flex-col lg:h-screen">
+	<div
+		class="flex h-auto min-h-14 flex-row items-center justify-between border-b border-neutral-200 px-6"
+	>
+		<div>logo</div>
+		<div>menus</div>
+	</div>
+	<div
+		class=" m-0 m-auto flex h-full w-auto flex-1 flex-col items-center justify-between gap-6 p-6 pb-0 lg:max-w-[1000px] lg:gap-20 lg:pt-20"
+	>
+		<div class=" flex flex-col gap-4">
+			<div class="text-center text-neutral-400">some small text goes here</div>
+			<div class="text-center text-3xl font-semibold lg:text-5xl lg:font-bold">
+				Ai powered fitness assistance for improve your life
+			</div>
+			<div class=" w-auto text-center text-base text-sm text-neutral-700 lg:text-base">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis at dicta temporibus
+				pariatur delectus quia repellendus, unde, nam accusantium ipsam totam.
+			</div>
+		</div>
+		<div
+			class="h-full min-h-[500px] w-full overflow-hidden rounded-tl-2xl rounded-tr-2xl bg-neutral-200"
+		>
+			image
+		</div>
+	</div>
+</div>
 
-<!-- main wrapper  -->
-<div
-	class="min-h-screen w-full bg-[url('https://images.unsplash.com/photo-1521080755838-d2311117f767?q=80&w=2762&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat"
-></div>
-
-<div>name</div>
+<div>
+	<div
+		class=" m-0 m-auto flex h-full max-w-300 flex-1 flex-col items-center justify-between gap-20 p-6"
+	>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque odio adipisci suscipit eos iste
+		nihil deserunt sequi pariatur provident molestiae, aliquid nobis quibusdam dicta! Provident
+		officiis dolores et cupiditate ab.
+	</div>
+</div>
